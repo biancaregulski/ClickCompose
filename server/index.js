1c0a7b1cbe37d95816193a6fc1e76d7c3d8f9ebb
biancaregulski <biancaregulski@yahoo.com>
2024-02-19 14:26:20 -0500

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+  res.send('Hello from the server!')
+})
+
+app.listen(8080, () => {
+  console.log('server listening on port 8080')
+})
